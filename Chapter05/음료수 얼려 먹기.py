35fba334539cda428e1867a5e3ab35dcf2844f36
eunseo2 <20180972@sungshin.ac.kr>
2021-06-29 23:07:34 +0900

    dfs

--- Chapter05/음료수 얼려 먹기.py
+++ Chapter05/음료수 얼려 먹기.py
@@ -1,32 +1,30 @@
-n, m = map(int, input().split())
-
-graph = []
-
+n,m = map(int, input().split())
+graph =[]
 
 for i in range(n):
     graph.append(list(map(int, input())))
 
 
 def dfs(x,y):
-    if x < 0 or x >=n or y <0 or y>= n :
+    if x >=n or x <0 or y <0 or y>=m:
         return False
     
-    if graph[x][y] == 0 : 
-        graph[x][y]  = 1 
+    if graph[x][y] == 0:
+       graph[x][y] = 1  
+       dfs(x-1,y) 
+       dfs(x,y-1)
+       dfs(x+1,y)
+       dfs(x,y+1) 
+       return True
     
-        dfs(x-1,y)
-        dfs(x,y-1)
-        dfs(x+1,y)
-        dfs(x,y+1) 
-        
-        return True 
-
+    else :
+        return False
 
 cnt = 0
+
 for i in range(n):
     for j in range(m):
-        if dfs(i,j) == True: 
+        if dfs(i,j) == True:
             cnt +=1 
-
 
 print(cnt)

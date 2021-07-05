b22e19b2f27d0b79f63f4033b96ca1604ed8b303
eunseo2 <20180972@sungshin.ac.kr>
2021-07-05 15:59:31 +0900

    stack

---
+++ programmers/Level1/크레인 인형뽑기 게임.py
@@ -0,0 +1,21 @@
+def solution(board, moves):
+    stack =[]
+    answer =0
+
+    for i in moves:
+        for j in range(len(board)):
+            if board[j][i-1] != 0:
+               stack.append(board[j][i-1])
+               board[j][i-1] =0
+
+
+               if len(stack) >= 2 :
+                    if stack[-1] == stack[-2]:
+                        stack.pop()
+                        stack.pop()
+                        answer +=2
+
+               
+               break
+
+    return answer
